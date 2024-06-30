af18aebacd37f61036888928178750c3eb3d033e
kannela <82623339+kannela@users.noreply.github.com>
2024-06-30 19:08:14 -0400

    interfaz añadir

--- main.py
+++ main.py
@@ -6,22 +6,27 @@ from kivymd.uix.button import MDRaisedButton
 from kivymd.uix.textfield import MDTextField
 from kivy.core.window import Window
 from kivymd.uix.toolbar import MDTopAppBar
+from kivy.uix.screenmanager import ScreenManager,Screen
 
+class Primera(Screen):
+    pass
 
+class Segunda(Screen):
+    pass
+class Tercera(Screen):
+    pass
 
+sm = ScreenManager()
+sm.add_widget(Primera(name='1'))
+sm.add_widget(Segunda(name='2'))
+sm.add_widget(Tercera(name='3'))
 
 
 class App(MDApp):
     def build(self):
 
-       
+        
         return Builder.load_file('main.kv')
 
-    def callback(self):
-        pass
-
-    def load_image(self):
-        image_path = self.root.ids.image_path.text
-        self.root.ids.image.source = image_path
 
 App().run()
