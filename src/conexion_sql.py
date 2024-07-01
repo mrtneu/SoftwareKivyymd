5b2b0703301b524a03605a797e40fc6f32076f40
kannela <82623339+kannela@users.noreply.github.com>
2024-07-01 17:33:50 -0400

    coneccion completada

--- src/conexion_sql.py
+++ src/conexion_sql.py
@@ -1,16 +1,26 @@
 from sqlalchemy import create_engine, text
 
+# Configuración de la conexión
+server = '34.176.110.254'
+database = 'ESTAMPILLA'
+username = 'sqlserver'
+password = 'admin'
 
-server = 'PC\\SQLEXPRESS'
-database = 'ESTAMPILLAS'
-driver = 'ODBC Driver 17 for SQL Server'
+# Cadena de conexión
+connection_string = f'mssql+pymssql://{username}:{password}@{server}/{database}'
 
-connection_string = f'mssql+pyodbc://@{server}/{database}?driver={driver}&trusted_connection=yes'
-
+# Crear el motor de SQLAlchemy
 engine = create_engine(connection_string)
 
+try:
+    with engine.connect() as connection:
+        # Ejemplo de consulta
+        query = "SELECT * FROM Estampillas"  # Aquí colocas tu consulta SQL
+        result = connection.execute(text(query))
+        
+        # Procesar los resultados
+        for row in result:
+            print(row)
 
-with engine.connect() as connection:
-    result = connection.execute(text("SELECT * FROM Estampillas"))
-    for row in result:
-        print(row)
+except Exception as e:
+    print(f"Error al ejecutar la consulta: {str(e)}")
