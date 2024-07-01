2265c0aae04a646719c4105059d39d6ee7ab4a1e
kannela <82623339+kannela@users.noreply.github.com>
2024-07-01 17:36:06 -0400

    bd en main

--- main.py
+++ main.py
@@ -7,6 +7,34 @@ from kivymd.uix.textfield import MDTextField
 from kivy.core.window import Window
 from kivymd.uix.toolbar import MDTopAppBar
 from kivy.uix.screenmanager import ScreenManager,Screen
+from sqlalchemy import create_engine, text
+
+# Configuración de la conexión
+server = '34.176.110.254'
+database = 'ESTAMPILLA'
+username = 'sqlserver'
+password = 'admin'
+
+# Cadena de conexión
+connection_string = f'mssql+pymssql://{username}:{password}@{server}/{database}'
+
+# Crear el motor de SQLAlchemy
+engine = create_engine(connection_string)
+
+try:
+    with engine.connect() as connection:
+        # Ejemplo de consulta
+        query = "SELECT * FROM Estampillas"  # Aquí colocas tu consulta SQL
+        result = connection.execute(text(query))
+        
+        # Procesar los resultados
+        for row in result:
+            print(row)
+
+except Exception as e:
+    print(f"Error al ejecutar la consulta: {str(e)}")
+
+
 
 class Primera(Screen):
     pass
