65349cfbe951e5839978980c6d3985fa33767ced
kannela <82623339+kannela@users.noreply.github.com>
2024-07-01 16:38:31 -0400

    db connected

--- src/conexion_sql.py
+++ src/conexion_sql.py
@@ -1,2 +1,16 @@
-import pyodbc
-print(pyodbc.version)
+from sqlalchemy import create_engine, text
+
+
+server = 'PC\\SQLEXPRESS'
+database = 'ESTAMPILLAS'
+driver = 'ODBC Driver 17 for SQL Server'
+
+connection_string = f'mssql+pyodbc://@{server}/{database}?driver={driver}&trusted_connection=yes'
+
+engine = create_engine(connection_string)
+
+
+with engine.connect() as connection:
+    result = connection.execute(text("SELECT * FROM Estampillas"))
+    for row in result:
+        print(row)
